6539a0f6f53911fea4c27f35fb147538e8d55dc2
b031vg <b031vg@yamaguchi-u.ac.jp>
2019-11-01 09:34:59 +0900

    ブランチ2ndにいるという文章を付けた

--- hello_world.c
+++ hello_world.c
@@ -4,5 +4,6 @@ int main(int argc, char **argv){
 	printf("hello world"\n);
 	printf("My hobby is watching youtube"\n);
 	printf("My hobby is playing tennis"\n);
+	printf("I am in branch2nd."\n);
 	return 0;
 }
